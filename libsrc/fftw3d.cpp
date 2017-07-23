fc66650567751344b70f15dec108293b32711da9
cppxfel <helenginn@Helen'sMacBookPro>
2017-07-23 18:47:16 +0100

    Improve ascii slice

--- libsrc/fftw3d.cpp
+++ libsrc/fftw3d.cpp
@@ -790,11 +790,11 @@ void cFFTW3d::printSlice(bool amplitude)
 				value = sqrt(getIntensity(i, j, 0));
 			}
 
-			if (value > 0.5) symbol = ".";
-			if (value > 0.5) symbol = ":";
-			if (value > 1.0) symbol = "\"";
-			if (value > 2.0) symbol = "*";
-			if (value > 4.0) symbol = "x";
+			if (value > 0.1) symbol = ".";
+			if (value > 0.2) symbol = ":";
+			if (value > 0.4) symbol = "\"";
+			if (value > 0.8) symbol = "*";
+			if (value > 3.0) symbol = "x";
 			if (value > 8.0) symbol = "H";
 			if (value > 16.0) symbol = "#";
 			if (value > 25.0) symbol = "@";
