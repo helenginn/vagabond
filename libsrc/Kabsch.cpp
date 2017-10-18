76fd4e2c114be642073bd4437cd0bc3a35b2a430
helenginn <helenginn@Helen'sMacBookPro>
2017-10-18 14:47:44 -0400

    LibICA (GPL v2) added to repo for SVD

---
+++ libsrc/Kabsch.cpp
@@ -0,0 +1,9 @@
+//
+//  Kabsch.cpp
+//  vagabond
+//
+//  Created by Helen Ginn on 18/10/2017.
+//  Copyright © 2017 Strubi. All rights reserved.
+//
+
+#include "Kabsch.hpp"
---
+++ libsrc/Kabsch.h
@@ -0,0 +1,14 @@
+//
+//  Kabsch.hpp
+//  vagabond
+//
+//  Created by Helen Ginn on 18/10/2017.
+//  Copyright © 2017 Strubi. All rights reserved.
+//
+
+#ifndef Kabsch_hpp
+#define Kabsch_hpp
+
+#include <stdio.h>
+
+#endif /* Kabsch_hpp */
