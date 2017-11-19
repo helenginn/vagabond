3a0fffcfde1bee7db427c96e65f718f8241efa68
helenginn <helenginn@Helen'sMacBookPro>
2017-11-19 13:21:55 +0000

    Adding seg fault handler

--- libsrc/main.cpp
+++ libsrc/main.cpp
@@ -7,10 +7,32 @@
 //
 
 #include "Options.h"
+#include <execinfo.h>
+#include <signal.h>
+
+
+void handler(int sig) {
+	void *array[10];
+	size_t size;
+
+	// get void*'s for all entries on the stack
+	size = backtrace(array, 10);
+
+	// print out all the frames to stderr
+	fprintf(stderr, "Error: signal %d:\n", sig);
+	backtrace_symbols_fd(array, (int)size, STDERR_FILENO);
+	exit(1);
+}
+
+
 
 int main(int argc, const char * argv[])
 {
 	/* Options are parsed and will generate the objects needed */
+
+	signal(SIGSEGV, handler);
+	signal(SIGABRT, handler);
+
 
 	Options options(argc, argv);
 	options.run();
