1dd2426d63f0e234b634f720ca62569880e4d655
helenginn <helenginn@Helen'sMacBookPro>
2018-01-17 17:23:34 +0000

    Change --help output

--- libsrc/Options.cpp
+++ libsrc/Options.cpp
@@ -123,11 +123,10 @@ void Options::displayHelp()
 	std::cout << "--with-pdb=<filename>\t\tName of the input PDB file to refine.\n" << std::endl;
 	std::cout << "--with-mtz=<filename>\t\tName of the MTZ file to refine.\n" << std::endl;
 	std::cout << "--output-dir=<directory>\tOptional name of a directory to dump processing.\n" << std::endl;
-	std::cout << "--target-flex=<num>\t\tOptional inherent flexibility to aim for in\n\t\t\t\tdegrees (default 0.001, disabled)\n" << std::endl;
 	std::cout << "--anchor-res=<num>\t\tOptional override default anchor residue for all\n\t\t\t\tchains (under development)\n" << std::endl;
 	std::cout << "--kick=<num>\t\t\tOptional override for kick fraction for initial bond\n" << std::endl;
 	std::cout << "--dampen=<num>\t\t\tOptional override for dampen fraction for all bonds\n" << std::endl;
-	std::cout << "--enable-tests\t\t\tEnable whatever it is Helen is currently working on\n" << std::endl;
+	std::cout << "--enable-tests=<num>\t\tEnable whatever it is Helen is currently working on\n" << std::endl;
 	std::cout << std::endl;
 	std::cout << "Baseline command to start running vagabond:\n" << std::endl;
 	std::cout << "\tvagabond --with-pdb=start.pdb --with-mtz=start.mtz\n" << std::endl;
