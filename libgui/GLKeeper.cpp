f8c0d0fe479a120f153d75d6993f3a21241ff434
helenginn <helenginn@Helen'sMacBookPro>
2018-01-29 14:19:33 +0000

    Tabbing but also incomplete fix for rotation problems

--- libgui/GLKeeper.cpp
+++ libgui/GLKeeper.cpp
@@ -23,50 +23,50 @@ bool GLKeeper::everMovedMouse = false;
 
 void GLKeeper::setupCamera(void)
 {
-	_translation = make_vec3(0, 0, START_Z);
-	camAlpha = 0;
-	camBeta = 0;
-	camGamma = 0;
-	_totalCentroid = make_vec3(0, 0, 0);
-	_centre = make_vec3(0, 0, START_Z);
+    _translation = make_vec3(0, 0, START_Z);
+    camAlpha = 0;
+    camBeta = 0;
+    camGamma = 0;
+    _totalCentroid = make_vec3(0, 0, 0);
+    _centre = make_vec3(0, 0, 0);
     zNear = 10;
     zFar = 100;
-	modelMat = make_mat4x4();
-	rotMat = make_mat4x4();
+    modelMat = make_mat4x4();
+    rotMat = make_mat4x4();
 
-	float correctedNear = zNear;
-	if (zNear <= 0.1) correctedNear = 0.1;
-	double side = 0.5;
-	float aspect = height / width;
-	projMat = mat4x4_frustum(side, -side, side * aspect, -side * aspect, correctedNear, zFar);
+    float correctedNear = zNear;
+    if (zNear <= 0.1) correctedNear = 0.1;
+    double side = 0.5;
+    float aspect = height / width;
+    projMat = mat4x4_frustum(side, -side, side * aspect, -side * aspect, correctedNear, zFar);
 
-	updateCamera();
+    updateCamera();
 }
 
 void GLKeeper::updateCamera(void)
 {
-	vec3 alteration = _objects[0]->fixCentroid(_totalCentroid);
-	_totalCentroid = _objects[0]->getCentroid();
+    vec3 alteration = _objects[0]->fixCentroid(_totalCentroid);
+    _totalCentroid = _objects[0]->getCentroid();
 
-	vec3 centre = _centre;
-	vec3 negCentre = centre;
-	vec3_mult(&centre, -1);
+    vec3 centre = _centre; 
+    vec3 negCentre = centre;
+    vec3_mult(&centre, -1);
 
-	mat4x4 change = make_mat4x4();
-	mat4x4_translate(&change, negCentre);
-	mat4x4_rotate(&change, camAlpha, camBeta, camGamma);
-	mat4x4_translate(&change, centre);
+    mat4x4 change = make_mat4x4();
+    mat4x4_translate(&change, negCentre);
+    mat4x4_rotate(&change, camAlpha, camBeta, camGamma);
+    mat4x4_translate(&change, centre);
 
-	mat4x4 transMat = make_mat4x4();
-	_centre = vec3_add_vec3(_centre, alteration);
-	_translation = vec3_add_vec3(_translation, alteration);
-	mat4x4_translate(&transMat, _translation);
+    mat4x4 transMat = make_mat4x4();
+    _centre = vec3_add_vec3(_centre, _translation);
+    _translation = vec3_add_vec3(_translation, alteration);
+    mat4x4_translate(&transMat, _translation);
 
-	mat4x4 tmp = mat4x4_mult_mat4x4(change, transMat);
-	modelMat = mat4x4_mult_mat4x4(modelMat, tmp);
+    mat4x4 tmp = mat4x4_mult_mat4x4(change, transMat);
+    modelMat = mat4x4_mult_mat4x4(modelMat, tmp);
 
-	camAlpha = 0; camBeta = 0; camGamma = 0;
-	_translation = make_vec3(0, 0, 0);
+    camAlpha = 0; camBeta = 0; camGamma = 0;
+    _translation = make_vec3(0, 0, 0);
 }
 
 
@@ -74,40 +74,40 @@ GLKeeper::GLKeeper(int newWidth, int newHeight)
 {
     width = newWidth;
     height = newHeight;
-	_rendered = false;
+    _rendered = false;
 
 #ifdef SETUP_BUFFERS
-	setupBuffers();
+    setupBuffers();
 #endif // SETUP_BUFFERS
 
-	Vagabond2GLPtr v2gl = Vagabond2GLPtr(new Vagabond2GL());
-	GLObjectPtr object = boost::static_pointer_cast<GLObject>(v2gl);
-	_totalCentroid = object->getCentroid();
+    Vagabond2GLPtr v2gl = Vagabond2GLPtr(new Vagabond2GL());
+    GLObjectPtr object = boost::static_pointer_cast<GLObject>(v2gl);
+    _totalCentroid = object->getCentroid();
 
-	_objects.push_back(object);
+    _objects.push_back(object);
 
-	initialisePrograms();
-	setupCamera();
+    initialisePrograms();
+    setupCamera();
 
-	glEnable(GL_BLEND);
+    glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void GLKeeper::render(void)
 {
-	glClearColor(0.9, 0.9, 0.9, 1.0);
+    glClearColor(0.9, 0.9, 0.9, 1.0);
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
 
-	updateCamera();
+    updateCamera();
 
-	for (int i = 0; i < _objects.size(); i++)
-	{
-		_objects[i]->setProjMat(projMat);
-		_objects[i]->setModelMat(modelMat);
-		_objects[i]->render();
-	}
+    for (int i = 0; i < _objects.size(); i++)
+    {
+        _objects[i]->setProjMat(projMat);
+        _objects[i]->setModelMat(modelMat);
+        _objects[i]->render();
+    }
 }
 
 void GLKeeper::cleanup(void)
@@ -122,9 +122,9 @@ void GLKeeper::keyPressed(char key)
 
 void GLKeeper::zoom(float x, float y, float z)
 {
-	_translation.x += x;
-	_translation.y += y;
-	_translation.z += z;
+    _translation.x += x;
+    _translation.y += y;
+    _translation.z += z;
 
 }
 
@@ -137,7 +137,7 @@ void GLKeeper::rotateAngles(float alpha, float beta, float gamma)
 
 void GLKeeper::panned(float x, float y)
 {
-	zoom(x / MOUSE_SENSITIVITY * 4, y / MOUSE_SENSITIVITY * 4, 0);
+    zoom(x / MOUSE_SENSITIVITY * 4, y / MOUSE_SENSITIVITY * 4, 0);
 }
 
 void GLKeeper::draggedLeftMouse(float x, float y)
@@ -153,9 +153,9 @@ void GLKeeper::draggedLeftMouse(float x, float y)
 
 void GLKeeper::draggedRightMouse(float x, float y)
 {
-	setShouldRender();
+    setShouldRender();
 
-	zoom(0, 0, -y / MOUSE_SENSITIVITY * 10);
+    zoom(0, 0, -y / MOUSE_SENSITIVITY * 10);
 }
 
 void GLKeeper::changeSize(int newWidth, int newHeight)
@@ -163,14 +163,14 @@ void GLKeeper::changeSize(int newWidth, int newHeight)
     width = newWidth;
     height = newHeight;
 
-	setShouldRender();
+    setShouldRender();
 }
 
 void GLKeeper::initialisePrograms()
 {
-	for (int i = 0; i < _objects.size(); i++)
-	{
-		_objects[i]->initialisePrograms();
-	}
+    for (int i = 0; i < _objects.size(); i++)
+    {
+        _objects[i]->initialisePrograms();
+    }
 }
 
