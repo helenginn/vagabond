d50caaa5590e0c538fb0b8eeb4efa326ddc58226
helenginn <helenginn@Helen'sMacBookPro>
2018-02-08 17:52:53 +0000

    Reformat Monomer.h

--- libsrc/Monomer.h
+++ libsrc/Monomer.h
@@ -19,93 +19,93 @@
 class Monomer : public AtomGroup
 {
 public:
-	Monomer();
-	void setup();
-	void tieAtomsUp();
+    Monomer();
+    void setup();
+    void tieAtomsUp();
 
-	/* For global modification of entire structure */
-	void setBackboneDampening(double value);
-	void setSidechainDampening(double value);
+    /* For global modification of entire structure */
+    void setBackboneDampening(double value);
+    void setSidechainDampening(double value);
 
-	bool isAfterAnchor();
+    bool isAfterAnchor();
 
-	MonomerPtr shared_from_this()
-	{
-		AtomGroupPtr group = AtomGroup::shared_from_this();
-		return boost::static_pointer_cast<Monomer>(group);
-	}
+    MonomerPtr shared_from_this()
+    {
+        AtomGroupPtr group = AtomGroup::shared_from_this();
+        return boost::static_pointer_cast<Monomer>(group);
+    }
 
-	void setIdentifier(std::string idString)
-	{
-		_identifier = idString;
-	}
+    void setIdentifier(std::string idString)
+    {
+        _identifier = idString;
+    }
 
-	std::string getIdentifier()
-	{
-		return _identifier;
-	}
+    std::string getIdentifier()
+    {
+        return _identifier;
+    }
 
-	BackbonePtr getBackbone()
-	{
-		return _backbone;
-	}
+    BackbonePtr getBackbone()
+    {
+        return _backbone;
+    }
 
-	SidechainPtr getSidechain()
-	{
-		return _sidechain;
-	}
+    SidechainPtr getSidechain()
+    {
+        return _sidechain;
+    }
 
-	void setResidueNum(int n)
-	{
-		_residueNum = n;
-		_sidechain->setResNum(_residueNum);
-	}
+    void setResidueNum(int n)
+    {
+        _residueNum = n;
+        _sidechain->setResNum(_residueNum);
+    }
 
-	int getResidueNum()
-	{
-		return _residueNum;
-	}
+    int getResidueNum()
+    {
+        return _residueNum;
+    }
 
-	void setPolymer(PolymerPtr poly)
-	{
-		_myPolymer = poly;
-	}
+    void setPolymer(PolymerPtr poly)
+    {
+        _myPolymer = poly;
+    }
 
-	PolymerPtr getPolymer()
-	{
-		return _myPolymer.lock();
-	}
+    PolymerPtr getPolymer()
+    {
+        return _myPolymer.lock();
+    }
 
-	virtual void addAtom(AtomPtr atom);
+    virtual void addAtom(AtomPtr atom);
 
-	void addModel(ModelPtr model)
-	{
-		_models.push_back(model);
-	}
+    void addModel(ModelPtr model)
+    {
+        _models.push_back(model);
+    }
 
-	int modelCount()
-	{
-		return _models.size();
-	}
+    int modelCount()
+    {
+        return _models.size();
+    }
 
-	ModelPtr model(int i)
-	{
-		return _models[i];
-	}
+    ModelPtr model(int i)
+    {
+        return _models[i];
+    }
 
-	void setKick(double value, bool beforeAnchor);
-	double getKick();
+    void setKick(double value, bool beforeAnchor);
+    double getKick();
 
-	std::string getResCode();
-	void setSideKick(double value);
+    std::string getResCode();
+    void setSideKick(double value);
 private:
-	std::string _identifier; // e.g. three-letter code
-	int _residueNum; // number in protein sequence including missing ones.
+    std::string _identifier; // e.g. three-letter code
+    int _residueNum; // number in protein sequence including missing ones.
 
-	PolymerWkr _myPolymer;
-	std::vector<ModelPtr> _models;
-	BackbonePtr _backbone;
-	SidechainPtr _sidechain;
+    PolymerWkr _myPolymer;
+    std::vector<ModelPtr> _models;
+    BackbonePtr _backbone;
+    SidechainPtr _sidechain;
 
 };
 
