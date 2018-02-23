9535944cc85ee21a9a7e179ab45ee557fe856251
Helen Ginn <helen@strubi.ox.ac.uk>
2018-02-23 20:09:16 +0000

    index on (no branch): 0d19007d Convert to OpenGL from Qt5

--- libgui/qtgui/MonomerExplorer.h
+++ libgui/qtgui/MonomerExplorer.h
@@ -7,7 +7,22 @@
 #include "AtomListItem.h"
 #include "SetterEdit.h"
 #include <QtWidgets/qpushbutton.h>
+#include <QtWidgets/qslider.h>
 #include "../GLKeeper.h"
+#include <map>
+
+typedef struct
+{
+    ParamOptionType optionType;
+    int scale;
+    double value;
+    int isZero;
+    QLabel *lOpt;
+    QLabel *lVal;
+    const char *unit;
+} ParamOption;
+
+typedef std::map<QSlider *, ParamOption> OptionMap;
 
 class MonomerExplorer : public QWidget
 {
@@ -24,7 +39,6 @@ public:
     {
         return _monomer;
     }
-
     GLKeeper *getKeeper()
     {
         return _keeper;
@@ -35,9 +49,14 @@ public:
 private slots:
     void clickedAtomListItem();
     void pushRefineDensity();
+    void pushRefineToEnd();
+    void setSliderValue();
 private:
     void initialise(MonomerPtr monomer);
     void populateList();
+    void makeRefinementButtons();
+    void makeSlider(ParamOptionType option, int num, QString name, int min, int max, int scale, int defVal, const char *unit);
+    void applyParamOptions(SamplerPtr sampled);
 
     MonomerPtr _monomer;
     GLKeeper *_keeper;
@@ -50,6 +69,7 @@ private:
     QLabel *_lDampen;
     QLabel *_lPhi;
     QLabel *_lPsi;
+    QLabel *_lRefineOpts;
 
     SetterEdit *_tTorsion;
     SetterEdit *_tKick;
@@ -58,7 +78,10 @@ private:
     SetterEdit *_tPsi;
 
     QPushButton *_bRefineDensity;
+    QPushButton *_bRefineToEnd;
     QLabel *_lCorrel;
+
+    OptionMap _optionMap;
 };
 
 
