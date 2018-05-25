93b8860e7b1b9f6c0d494ca110fc8c41c9c7aa49
Helen Ginn <helen@strubi.ox.ac.uk>
2018-05-25 12:17:40 +0200

    Fix problem if COORDVAL_FULL is defined

--- libsrc/AtomGroup.cpp
+++ libsrc/AtomGroup.cpp
@@ -892,11 +892,11 @@ double AtomGroup::scoreFinalMap(CrystalPtr crystal, FFTPtr segment,
 			double fc = vals[i].fc;
 			double mask = 0;
 			vec3 pos = make_vec3(0, 0, 0);
-			
-#ifdef COORDVAL_FULL
+
+			#ifdef COORDVAL_FULL
 			mask = vals[i].mask;
-			vec3 pos = vals[i].pos;
-#endif
+			pos = vals[i].pos;
+			#endif
 
 			if (fc < cutoff) continue;
 
