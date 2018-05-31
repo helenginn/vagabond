ee4eb859ab78f053a8ed88620331bc24a41ee2b3
Helen Ginn <helen@strubi.ox.ac.uk>
2018-05-31 16:38:10 +0200

    Change calculation of extra rotations

--- libsrc/Polymer.cpp
+++ libsrc/Polymer.cpp
@@ -1128,9 +1128,13 @@ void Polymer::calculateExtraRotations()
 		double cosine = vec3_cosine_with_vec3(diff, _magicRotAxis);
 		vec3 cross = vec3_cross_vec3(diff, _magicRotAxis);
 
-		// Need to make sure the poles are opposite. 
-		// Choice of doing so is relatively arbitrary.
+		/* If the angle is far, make it more extreme to simulate
+		 * a bell curve. */
 		double angleMult = cosine;
+		double mult = exp(angleMult * _rotExponent);
+		angleMult *= mult;
+
+		// Need to make sure the poles are opposite. 
 		if (cross.x < 0)
 		{
 			angleMult *= -1;
