c281f02ea6ad643b5772897774cac06f5e660c14
Helen Ginn <helen@strubi.ox.ac.uk>
2018-06-07 14:34:08 +0200

    Add B factor agreement as an option

--- libsrc/MapScoreWorkspace.h
+++ libsrc/MapScoreWorkspace.h
@@ -12,8 +12,9 @@ typedef enum
 	ScoreTypeMultiply = 1, /** Weighted (by model) sum of map voxels */
 	ScoreTypeRFactor = 2, /** R factor in real space for electron density */
 	ScoreTypeModelRMSDZero = 3, /** All ensemble positions against PDB */
-	ScoreTypeModelPos = 4, /** Average ensemble position against PDB */
-	ScoreTypeRMSDZero = 5, /** Sum of squares of anisotropic tensor */
+	ScoreTypeBFactorAgreement = 4, /** All ensemble B factors against PDB */
+	ScoreTypeModelPos = 5, /** Average ensemble position against PDB */
+	ScoreTypeRMSDZero = 6, /** Sum of squares of anisotropic tensor */
 } ScoreType;
 
 typedef struct 
