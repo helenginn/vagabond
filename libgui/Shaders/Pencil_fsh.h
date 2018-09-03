4e778ed1683c0eaf3f7ff199ede24fc8967463e6
Helen Ginn <github@hginn.co.uk>
2018-09-03 15:09:18 +0200

    Make slightly more fully transparent areas, and add a foreground clipping of -5

--- libgui/Shaders/Pencil_fsh.h
+++ libgui/Shaders/Pencil_fsh.h
@@ -12,14 +12,18 @@ std::string Pencil_fsh =
 "\n"\
 "void main()\n"\
 "{\n"\
+"	if (vPos[2] > -5.) {\n"\
+"		discard;\n"\
+"	}\n"\
 "	vec3 pos = vec3(vPos[0], vPos[1], vPos[2]);\n"\
 "	vec3 lightDir = normalize(pos - light_pos);\n"\
 "	float diff = abs(dot(vNormal, lightDir));\n"\
 "	diff = sqrt(1. - diff * diff);\n"\
-"   if (diff < 0.3) {\n"\
+"   float cutoff = 0.4;\n"\
+"   if (diff < cutoff) {\n"\
 "       diff = 0.;\n"\
 "	} else {\n"\
-"	    diff -= 0.3; diff /= 0.7;\n"\
+"	    diff -= cutoff; diff /= 1. - cutoff;\n"\
 "	}\n"\
 "	vec2 tex = vec2(vPos[0], vPos[1]);\n"\
 "	vec4 temp = texture2D(pencilTexture, tex);\n"\
