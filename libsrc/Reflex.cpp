09aeee40a90413932b6bff3ad663d29e0703c92e
Helen Ginn <github@hginn.co.uk>
2018-10-02 12:39:54 +0200

    Add Reflex cpp/h files

---
+++ libsrc/Reflex.cpp
@@ -0,0 +1,254 @@
+//
+//  Reflex.cpp
+//  vagabond
+//
+//  Created by Helen Ginn, 2018
+//  Copyright © 2018 Helen Ginn. All rights reserved.
+//
+
+#include "Reflex.h"
+#include "Shouter.h"
+#include "Polymer.h"
+#include "FileReader.h"
+#include "Monomer.h"
+#include "CSV.h"
+#include "Backbone.h"
+#include "Options.h"
+#include "AtomGroup.h"
+#include "RefinementNelderMead.h"
+
+Reflex::Reflex()
+{
+	_pieceCount = 1;
+	_pieceType = PieceTypeResidue;
+	_bFactor = 0;
+	_kAbs = 1;
+	_intercept = 0;
+	_gradient = 1;
+	_aniso = RefineMat3x3Ptr(new RefineMat3x3(this, NULL));
+	_aniso->setZero();
+}
+
+void Reflex::segmentPolymer()
+{
+	/* Divide the polymer into sections */
+	
+	for (size_t i = _polymer->monomerBegin(); 
+	     i <= _polymer->monomerCount() - _pieceCount; i++)
+	{
+		AtomGroupPtr segment = AtomGroupPtr(new AtomGroup());
+
+		for (int j = i; j < i + _pieceCount; j++)
+		{
+			MonomerPtr monomer = _polymer->getMonomer(j);
+			
+			if (!monomer)
+			{
+				continue;
+			}
+			
+			segment->addAtomsFrom(monomer);
+		}
+
+		_segments.push_back(segment);
+	}
+}
+
+double Reflex::bFactorScore(void *object)
+{
+	Reflex *me = static_cast<Reflex *>(object);
+	double b = me->_bFactor;
+	double abs = me->_kAbs;
+	FFTPtr obs = me->_obs;
+	FFTPtr calc = me->_calc;
+
+	mat3x3 inv = obs->getBasis();	
+	mat3x3_scale(&inv, obs->nx, obs->ny, obs->nz);
+	mat3x3 basis = mat3x3_inverse(inv);
+	DiffractionPtr data = Options::getRuntimeOptions()->getActiveData();
+	double res = 0;
+	res = me->_workspace.crystal->getMaxResolution(data);
+	//res = 1 / (1 / res * 4);
+	res = 1 / (1 / res * 2);
+	std::vector<double> bins, obsAves, calcAves;
+	const int binnum = 20;
+	generateResolutionBins(0, res, binnum, &bins);
+	obsAves = std::vector<double>(binnum, 0);
+	calcAves = std::vector<double>(binnum, 0);
+	std::vector<int> counts = std::vector<int>(binnum, 0);
+
+	std::vector<double> xs, ys;
+	mat3x3 aniso = me->_aniso->getMat3x3();
+	
+	for (int k = 0; k < obs->nz; k++)
+	{
+		for (int j = 0; j < obs->ny; j++)
+		{
+			for (int i = 0; i < obs->nx; i++)
+			{
+				vec3 ijk = make_vec3(i, j, k);
+				mat3x3_mult_vec(basis, &ijk);
+				double length = vec3_length(ijk);
+				
+				double d = 1 / length;
+				double four_d_sq = (4 * d * d);
+				if (d < res)
+				{
+					continue;
+				}
+				
+				double isoBMod = exp(-b / four_d_sq);
+
+				long ele = obs->element(i, j, k);
+
+				float fCalc = sqrt(calc->getIntensity(ele)) * isoBMod;
+				float fObs = sqrt(obs->getIntensity(ele));
+				
+				xs.push_back(fCalc);
+				ys.push_back(fObs);
+			}
+		}
+	}
+
+	double rFac = r_factor(xs, ys);
+
+	return rFac;
+}
+
+void Reflex::prepareSegments(AtomGroupPtr segment, bool preprocess)
+{
+	CrystalPtr crystal = Options::getRuntimeOptions()->getActiveCrystal();
+	CSym::CCP4SPG *spg = CSym::ccp4spg_load_by_standard_num(1);
+	
+	_workspace.scoreType = ScoreTypeCorrel;
+	_workspace.crystal = crystal;
+	_workspace.selectAtoms = segment->getAtoms();
+	_workspace.segment = FFTPtr();
+	_workspace.ave = empty_vec3();
+	_workspace.basis = make_mat3x3();
+	_workspace.flag = MapScoreFlagNone;
+	
+	double score = AtomGroup::scoreWithMapGeneral(&_workspace);
+	FFTPtr calcSegment = FFTPtr(new FFT(*_workspace.segment));
+
+	calcSegment->shiftToCentre();
+	
+	_workspace.flag = MapScoreFlagReplaceWithObs;
+	AtomGroup::scoreWithMapGeneral(&_workspace);
+	FFTPtr obsSegment = _workspace.segment;
+	_calcSize = calcSegment->averageAll();
+
+	if (preprocess)
+	{
+		obsSegment->fft(1);
+		calcSegment->fft(1);
+
+		for (int i = 0; i < calcSegment->nn; i++)
+		{
+			calcSegment->data[i][0] *= _gradient;
+			calcSegment->data[i][0] += _intercept;
+		}
+
+		_calcSize = obsSegment->averageAll();
+		std::cout << "Obs ave: " << obsSegment->averageAll() << std::endl;
+		std::cout << "Calc ave: " << calcSegment->averageAll() << std::endl;
+	}
+	
+	_obs = obsSegment;
+	_calc = calcSegment;
+}
+
+void Reflex::refineFlex()
+{	
+	_bFactor = 0;
+	_aniso->setZero();
+	
+	NelderMeadPtr nm;
+	nm = NelderMeadPtr(new RefinementNelderMead());
+	nm->setJobName("segment_" + i_to_str(_round));
+	nm->setEvaluationFunction(bFactorScore, this);
+	nm->setCycles(20);
+	nm->addParameter(this, getBFactor, setBFactor, 5, 0.1, "b");
+	nm->refine();
+	nm->clearParameters();
+}
+
+void Reflex::findCalcScale()
+{
+	prepareSegments(_polymer, false);
+	
+	_calc->fft(1);
+	_obs->fft(1);
+
+	std::vector<double> xs, ys;
+
+	std::cout << "Calc ave: " << _calc->averageAll() << std::endl;
+	std::cout << "Obs ave: " << _obs->averageAll() << std::endl;
+	
+	double calc_ave = _calc->averageAll();
+	double obs_ave = _obs->averageAll();
+
+	for (int i = 0; i < _obs->nn; i++)
+	{
+		double fo = _obs->data[i][0];
+		double fc = _calc->data[i][0];
+		
+		xs.push_back(fc);
+		ys.push_back(fo);
+	}
+	
+	_gradient = scale_factor(xs, ys);
+	_intercept = 0;
+	_gradient = obs_ave / calc_ave;
+	std::cout << "Gradient: " << _gradient << std::endl;
+}
+
+void Reflex::calculate()
+{
+	if (_pieceType != PieceTypeResidue)
+	{
+		shout_at_helen("Only support residue flex estimation");
+	}
+	
+	std::cout << "Scanning polymer for new flexibility info." << std::endl;
+	
+	segmentPolymer();
+	findCalcScale();
+	
+	std::cout << "Segment count: " << _segments.size() << std::endl;
+	CSVPtr csv = CSVPtr(new CSV(2, "seg", "calc_ave"));
+	
+	std::vector<double> refineBs;
+	
+	for (int i = 0; i < _segments.size(); i++)
+	{
+		_round = i + _pieceCount / 2;
+		prepareSegments(_segments[i]);
+		refineFlex();
+		refineBs.push_back(_bFactor);
+		csv->addEntry(2, (double)_round, _bFactor);
+	}
+		
+	for (int i = _pieceCount / 2; false && i < _segments.size() - _pieceCount / 2; i++)
+	{
+		double ave = 0;
+		int count = 0;
+
+		for (int j = 0; j < _pieceCount; j++)
+		{
+			if (i + j >= _segments.size())
+			{
+				continue;
+			}
+			
+			ave += refineBs[i + j];
+			count++;
+		}
+		
+		ave /= (double)count;
+		
+		csv->addEntry(2, (double)i + _pieceCount / 2, ave);
+	}
+	
+	csv->writeToFile(_polymer->getChainID() + "_bfactor_targets.csv");
+}
