cd4e678a43527ab4b869da206d574fee4bcf9bbf
Helen Ginn <helen@strubi.ox.ac.uk>
2019-01-13 13:35:38 +0000

    GPL notices

--- libsrc/Crystal.cpp
+++ libsrc/Crystal.cpp
@@ -1,10 +1,20 @@
-//
-//  Crystal.cpp
-//  vagabond
-//
-//  Created by Helen Ginn on 13/07/2017.
-//  Copyright (c) 2017-8 Helen Ginn. All rights reserved.
-//
+// Vagabond
+// Copyright (C) 2017-2018 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
 
 #include <sstream>
 #include <iostream>
