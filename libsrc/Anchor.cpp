82c354516ea86badfbeea96519debe584517375d
Helen Ginn <github@hginn.co.uk>
2019-04-14 17:45:57 +0200

    Changes to the way rotations are applied so effects of rotation are identical on basis and start positions

--- libsrc/Anchor.cpp
+++ libsrc/Anchor.cpp
@@ -242,7 +242,6 @@ void Anchor::rotateBases()
 			                                  _storedSamples[i].basis); 
 			
 			rot_only = mat3x3_mult_mat3x3(rot_mat, rot_only);
-			_storedSamples[i].basis = basis;
 		}
 		
 		vec3 diff_to_old = vec3_subtract_vec3(_storedSamples[i].old_start,
@@ -250,6 +249,8 @@ void Anchor::rotateBases()
 		mat3x3_mult_vec(rot_only, &diff_to_old);
 		vec3 new_old = vec3_add_vec3(start, diff_to_old);
 		_storedSamples[i].old_start = new_old;
+		mat3x3 basis = mat3x3_mult_mat3x3(_storedSamples[i].basis, rot_only);
+		_storedSamples[i].basis = basis;
 	}
 }
 
