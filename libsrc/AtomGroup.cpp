eb1cfb013a4ee7e14d395ee3192296a9a9e1a8f0
Helen Ginn <github@hginn.co.uk>
2019-04-30 11:01:18 +0200

    Return initial estimation to half-resolution

--- libsrc/AtomGroup.cpp
+++ libsrc/AtomGroup.cpp
@@ -929,7 +929,7 @@ vec3 AtomGroup::prepareCubicMap(FFTPtr *scratchFull, vec3 *offset,
                                 vec3 min, vec3 max, double buff)
 {
 	double maxDStar = Options::getRuntimeOptions()->getActiveCrystalDStar();
-	double cubeDim = 0.67 / (maxDStar);
+	double cubeDim = 1.0 / (maxDStar);
 	
 	if (cubeDim > 1)
 	{
