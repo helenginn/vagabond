78eb5d805670e5f9ea64fd0efc2256a5f2027de5
Helen Ginn <github@hginn.co.uk>
2019-05-18 14:10:29 +0200

    Add basic WeightedMap class for supplying electron density generation variants

---
+++ libsrc/WeightedMap.h
@@ -0,0 +1,30 @@
+// Vagabond
+// Copyright (C) 2017-2018 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__weighted_map__
+#define __vagabond__weighted_map__
+
+class WeightedMap
+{
+public:
+	WeightedMap();
+
+private:
+};
+
+#endif
