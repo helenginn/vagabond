a8b06dbb267fb94985271eec67e81c89b3208474
Helen Ginn <github@hginn.co.uk>
2019-09-02 13:10:47 +0200

    Add FreeRflag to list of possible flags; used by XDSApp

--- libsrc/DiffractionMTZ.cpp
+++ libsrc/DiffractionMTZ.cpp
@@ -127,6 +127,7 @@ void DiffractionMtz::load()
 	rFreeNames.push_back("FREER");
 	rFreeNames.push_back("FREE");
 	rFreeNames.push_back("FreeR_flag");
+	rFreeNames.push_back("FreeRflag");
 
 	getCol(rFreeNames, mtz, &col_rfree);
 
