65f6029e32d4d76423626e741167c738b8bc0a5d
Helen Ginn <github@hginn.co.uk>
2020-02-20 20:44:57 +0100

    Add more voxels for calculation depending on resolution

--- libsrc/AtomGroup.cpp
+++ libsrc/AtomGroup.cpp
@@ -888,6 +888,7 @@ void AtomGroup::prepareCubicMap(VagFFTPtr *scratchFull, vec3 min, vec3 max,
 	
 	/* 2 Angstroms buffer region on either side of the protein */
 	double buff = BUFFER_REGION;
+	buff += cubeDim * 3;
 	vec3 buffer = make_vec3(buff, buff, buff);
 	vec3_subtract_from_vec3(&min, buffer);
 
