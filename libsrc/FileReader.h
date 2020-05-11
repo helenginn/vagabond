bcb7dfc6ac4b133187ca6dabc2f8d3a608dbb02f
Helen Ginn <github@hginn.co.uk>
2020-05-11 16:18:21 +0200

    Add globbing to FileReader

--- libsrc/FileReader.h
+++ libsrc/FileReader.h
@@ -9,6 +9,11 @@
 #ifndef __FileReader__
 #define __FileReader__
 
+#include <glob.h> // glob(), globfree()
+#include <stdexcept>
+#include <cstring>
+#include <sstream>
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -40,6 +45,44 @@ void to_lower(std::string &str);
 void to_upper(std::string &str);
 
 void print_cc_diff(double diff, int limit);
+
+inline std::vector<std::string> glob(const std::string& pattern) {
+	using namespace std;
+
+	// glob struct resides on the stack
+	glob_t glob_result;
+	memset(&glob_result, 0, sizeof(glob_result));
+
+	// do the glob operation
+	int return_value = glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_result);
+	if (return_value != 0) 
+	{
+		globfree(&glob_result);
+
+		if (return_value == GLOB_NOMATCH)
+		{
+			return std::vector<std::string>();
+		}
+
+		stringstream ss;
+		ss << "glob() failed with return_value " << return_value << endl;
+		throw std::runtime_error(ss.str());
+	}
+
+	// collect all the filenames into a std::list<std::string>
+	vector<string> filenames;
+
+	for (size_t i = 0; i < glob_result.gl_pathc; i++)
+	{
+		filenames.push_back(string(glob_result.gl_pathv[i]));
+	}
+
+	// cleanup
+	globfree(&glob_result);
+
+	// done
+	return filenames;
+}
 
 class FileReader
 {
